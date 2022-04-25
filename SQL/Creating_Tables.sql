1279d66190e945fb68c71e7665c7edd433cdcbe2
Wrancher123 <99512669+Wrancher123@users.noreply.github.com>
2022-04-25 11:29:54 -0500

    Updating

--- SQL/Creating_Tables.sql
+++ SQL/Creating_Tables.sql
@@ -7,6 +7,8 @@ CREATE TABLE departments (
      UNIQUE (dept_name)
 );
 
+SELECT * FROM departments;
+
 -- Employees
 CREATE TABLE employees (
      emp_no INT NOT NULL,
@@ -17,6 +19,8 @@ CREATE TABLE employees (
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
 );
+
+SELECT * FROM employees;
 
 -- Dept Manager
 CREATE TABLE dept_manager (
@@ -29,6 +33,7 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+SELECT * FROM dept_manager;
 
 --Salaries
 CREATE TABLE salaries (
@@ -37,13 +42,15 @@ CREATE TABLE salaries (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  	PRIMARY KEY (emp_no)
 );
 
---Dept Emp
+SELECT * FROM salaries;
+
+--Dept Emp(*********************)
 CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
@@ -52,6 +59,9 @@ FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+SELECT * FROM dept_emp;
+
+
 --Titles
 CREATE TABLE titles (
   emp_no INT NOT NULL,
@@ -59,7 +69,9 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY(emp_no)
+	PRIMARY KEY(emp_no, from_date)
 );
 
-SELECT * FROM departments
+SELECT * FROM titles;
+
+
